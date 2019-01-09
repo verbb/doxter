8c1e356c279ec8e58876b334affe9eaaacc0f81f
Selvin Ortiz <selvin@selvin.co>
2019-01-09 07:40:07 -0600

    Update example to show shortcode tag registration

---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    // Register shortcode tag(s) to template mapping
+    'shortcodes' => [
+        'tags' => [
+            'audio' => '_doxter/shortcode/audio',
+            'img:image' => '_doxter/shortcodes/image',
+            'vimeo:youtube' => '_doxter/shortcodes/video',
+        ]
+    ]
+];
