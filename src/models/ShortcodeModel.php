85e9540d3de5ef8914c253bf77d1a29b977e287e
Selvin Ortiz <selvin@selvin.co>
2019-01-09 08:36:57 -0600

    Update `ShortcodeModel` with `__get()` and doc blocks

--- src/models/ShortcodeModel.php
+++ src/models/ShortcodeModel.php
@@ -37,21 +37,41 @@ use selvinortiz\doxter\common\parsers\Shortcode;
  */
 class ShortcodeModel extends Model
 {
+    /**
+     * Shortcode name/abbreviation
+     * e.g. [img] 👉 "img"
+     *
+     * @var string
+     */
     public $name;
+
+    /**
+     * All key/value pairs found in the shortcode definition
+     * e.g. [img src=image.png size=large] 👉 ['src' => 'image.png', 'size' => 'large']
+     *
+     * @var array
+     */
     public $params;
+
+    /**
+     * Content between start and end shortcode tags
+     * e.g. [quote]Quote text goes here...[/quote]
+     *
+     * @var string
+     */
     public $content;
 
     /**
-     * Returns the value of an attribute if found or $default value
+     * Returns a parameter value if one is found
+     * Useful when accessing via twig
      *
-     * @param string     $attribute
-     * @param null|mixed $default
+     * @param string $param
      *
-     * @return null|mixed
+     * @return mixed|null
      */
-    public function get($attribute, $default = null)
+    public function __get($param)
     {
-        return isset($this->{$attribute}) ? $this->{$attribute} : $default;
+        return $this->getParam($param);
     }
 
     /**
@@ -64,17 +84,24 @@ class ShortcodeModel extends Model
      */
     public function getParam($name, $default = null)
     {
-        return isset($this->params[$name]) ? $this->params[$name] : $default;
+        return $this->params[$name] ?? $default;
     }
 
+    /**
+     * @return mixed|string
+     */
     public function parseContent()
     {
-        if (!empty($this->content)) {
-            if (strpos($this->content, '[') !== false || strpos($this->content, '{') !== false) {
-                return Shortcode::instance()->parse($this->content);
-            }
-
-            return $this->content;
+        if (empty($this->content))
+        {
+            return '';
         }
+
+        if (mb_stripos($this->content, '[') !== false || mb_stripos($this->content, '{') !== false)
+        {
+            return Shortcode::instance()->parse($this->content);
+        }
+
+        return $this->content;
     }
 }
