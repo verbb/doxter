e6abe96feed046617446cba94d8f1efe569ac2dd
Selvin Ortiz <selvin@selvin.co>
2019-01-10 21:43:58 -0600

    Refactor TOC generator and return an array of items rather than html

--- src/common/parsers/Toc.php
+++ src/common/parsers/Toc.php
@@ -28,6 +28,11 @@ class Toc extends BaseParser
         return $this->getToc($source);
     }
 
+    /**
+     * @param $source
+     *
+     * @return array
+     */
     protected function getToc($source)
     {
         $toc = [];
@@ -36,41 +41,48 @@ class Toc extends BaseParser
         $source = str_replace(["\r\n", "\r"], "\n", $source);
 
         preg_match_all(
-            '/^(?:=|-|#).*$/m',
+            '/^(?:=|#).*$/m',
             $source,
             $matches,
             PREG_PATTERN_ORDER | PREG_OFFSET_CAPTURE
         );
 
-        // preprocess: iterate matched lines to create an array of items
-        // where each item is an array(level, text)
         $sourceLength = strlen($source);
+
         foreach ($matches[0] as $item)
         {
             $mark = substr($item[0], 0, 1);
 
             if ($mark == '#')
             {
-                $item_text  = $item[0];
-                $item_level = strrpos($item_text, '#') + 1;
-                $item_text  = substr($item_text, $item_level);
+                $text  = $item[0];
+                $level = strrpos($text, '#') + 1;
+                $text  = substr($text, $level);
             }
             else
             {
-                // text is the previous line (empty if <hr>)
-                $item_offset      = $item[1];
-                $prev_line_offset = strrpos($source, "\n", -($sourceLength - $item_offset + 2));
-                $item_text        =
-                    substr($source, $prev_line_offset, $item_offset - $prev_line_offset - 1);
-                $item_text        = trim($item_text);
-                $item_level       = $mark == '=' ? 1 : 2;
+                // Text is the previous line (empty if <hr>)
+                $offset     = $item[1];
+                $prevOffset = strrpos($source, "\n", -($sourceLength - $offset + 2));
+                $text       = substr($source, $prevOffset, $offset - $prevOffset - 1);
+                $text       = trim($text);
+                $level      = $mark == '=' ? 1 : 2;
             }
-            if (!trim($item_text) || strpos($item_text, '|') !== false)
+
+            if (!trim($text) || strpos($text, '|') !== false)
             {
-                // item is an horizontal separator or a table header, don't mind
+                // Item is an horizontal separator or a table header, don't mind
                 continue;
             }
-            $toc[] = ['level' => $item_level, 'slug' => ElementHelper::createSlug(trim($item_text)), 'text' => trim($item_text)];
+
+            $id = ElementHelper::createSlug(trim($text));
+
+            $toc[] = [
+                'id'    => $id,
+                'text'  => trim($text),
+                'hash'  => '#'.$id,
+                'level' => $level,
+            ];
         }
 
         return $toc;
